a304a74d21f371cd992c16d4a09953333b6cbd5b
AudreyDike <adts222@gmail.com>
2024-10-07 20:17:37 -0700

    Updated lab20

--- lab20.cpp
+++ lab20.cpp
@@ -13,4 +13,21 @@ private:
     double* prices;
 
 public:
-    
+  // Default constructor
+    Chair() {
+        prices = new double[SIZE];
+        legs = (rand() % 2) + 3; // Randomly assign 3 or 4 legs
+        for (int i = 0; i < SIZE; i++) {
+            prices[i] = (rand() % (MAX - MIN + 1) + MIN) / (double)100; // Random prices between 100 and 999.99
+        }
+    }
+
+    // Parameter constructor with legs and prices
+    Chair(int l, double* p) {  // Accept pointer to array
+        prices = new double[SIZE];
+        legs = l;
+        for (int i = 0; i < SIZE; i++) {
+            prices[i] = p[i];
+        }
+    }
+
