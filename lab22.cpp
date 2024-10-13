abbe65878e00c6e0ee92dacbfaccd6cf38948882
AudreyDike <adts222@gmail.com>
2024-10-13 15:31:55 -0700

    lab22.cpp

---
+++ lab22.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
+
+class DoublyLinkedList {
+private:
+    struct Node {
+        int data;
+        Node* prev;
+        Node* next;
+        Node(int val, Node* p = nullptr, Node* n = nullptr) {
+            data = val;
+            prev = p;
+            next = n;
+        }
+    };
+    Node* head;
+    Node* tail;
+
+public:
+    // Constructor
+    DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+
+    // Push to the back of the list
+    void push_back(int value) {
+        Node* newNode = new Node(value);
+        if (!tail) { // if there's no tail, the list is empty
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+
